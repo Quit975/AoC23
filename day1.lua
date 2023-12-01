4fdcfd1791ee8f33db23ec1caab91417b0fefd5b
Quit975 <plushquotient@gmail.com>
2023-12-01 18:14:21 +0100

    Day 1 star 2 - lol what is this xD

--- day1.lua
+++ day1.lua
@@ -33,8 +33,66 @@ function SolveSecondPart()
     number_converter["eight"] = 8
     number_converter["nine"] = 9
 
-    for line in input_file:lines() do
+    local FindFirstDigit = function(in_string, digit_to_look)
+        local search_pattern = digit_to_look and tostring(digit_to_look) or "%d";
+        local idx = string.find(in_string, search_pattern);
+        return idx, tonumber(string.sub(in_string, idx, idx));
+    end
 
+    local FindLastDigit = function(in_string, digit_to_look)
+        local search_pattern = digit_to_look and tostring(digit_to_look) or "%d";
+        local idx = string.find(string.reverse(in_string), search_pattern);
+        idx = string.len(in_string) - idx + 1;
+        return idx, tonumber(string.sub(in_string, idx, idx));
+    end
+
+    local FindFirstWordDigit = function(in_string)
+        local first_idx = 1000;
+        local first_word = "";
+
+        for k in pairs(number_converter) do
+            local search_idx, search_len = string.find(in_string, k);
+            if not search_idx then goto continue end
+            if search_idx < first_idx then
+                first_idx = search_idx;
+                first_word = string.sub(in_string, search_idx, search_len);
+            end
+            ::continue::
+        end
+
+        return first_idx, number_converter[first_word];
+    end
+
+    local FindLastWordDigit = function(in_string)
+        local last_idx = 0;
+        local last_word = "";
+
+        for k in pairs(number_converter) do
+            local _, search_idx = string.find(string.reverse(in_string), string.reverse(k));
+            if not search_idx then goto continue end
+            search_idx = string.len(in_string) - search_idx + 1;
+            local end_idx = search_idx + (string.len(k) - 1);
+            if search_idx > last_idx then
+                last_idx = search_idx;
+                last_word = string.sub(in_string, search_idx, end_idx);
+            end
+            ::continue::
+        end
+
+        return last_idx, number_converter[last_word];
+    end
+
+    for line in input_file:lines() do
+        local first_digit_index, first_digit_value = FindFirstDigit(line);
+        local last_digit_index, last_digit_value = FindLastDigit(line);
+        local first_word_index, first_word_value = FindFirstWordDigit(line);
+        local last_word_index, last_word_value = FindLastWordDigit(line);
+
+        local final_left = first_digit_index < first_word_index and first_digit_value or first_word_value;
+        local final_right = last_digit_index > last_word_index and last_digit_value or last_word_value;
+        local final_num = tonumber(string.format("%d%d", final_left, final_right));
+        solution = solution + final_num;
+        --print(string.format("First %d last %d", first_digit_index, last_digit_index));
     end
     input_file:seek("set", 0);
 
